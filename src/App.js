a65c293ca0b12335ca82b3d60dad2203145e4e42
OmarMWarraich <earthling986@gmail.com>
2022-02-07 20:02:24 +0500

    added components
    Added BookShelf.js and BookList.js and rendered the later in App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,61 @@
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
 import React, { Component } from 'react';
 import * as BooksAPI from "./BooksAPI";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import BookList from "./components/BookList";
 
+/** @description Main App component. */
 
 class App extends Component {
   state = {
     books: []
   };
 
+ /**
+ *  @Lifecycle event handler called just after the app loads into the DOM.
+ *  Call the API to get all the books.
+ */
+
   componentDidMount() {
     this.updateData();
   }
   
+ /** Update to handle shelf change*/
+
   updateShelf = (book, shelf) => {
     BooksAPI.update(book, shelf).then(response => {
       this.updateData();
     })
   }
+
+  /** Update the book in the state*/
+
   updateData = () => {
-    BooksAPI.getAll().then(data => console.log(data));
+    BooksAPI.getAll().then(data => {
+      this.setState({
+        books: data
+      })
+    });
   }
 
 
-render() {
-  return (
-    <div></div>
-  )
-}
+  render() {
+    return (
+      <div className="bg-lime-600 pb-0">
+        <Router>
+          <Routes>
+            {/*For current app*/}
+            <Route
+              exact
+              path="/"
+              element={<BookList currentBooks={this.state.books} />}
+            />
+            
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
 
 };
 
---
+++ src/components/BookList.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import * as BooksAPI from "../BooksAPI";
+import BookShelf from "./BookShelf";
+
+/** @description: BookList Component */
+
+class BookList extends React.Component {
+    state = {};
+
+/** @function: Filter the books as per shelf*/
+updateShelf = (bookId, event) => {
+    let currentBooks = this.props.currentBooks;
+    const book = currentBooks.filter((book) => book.id === bookId)[0];
+    book.shelf = event.target.value;
+    BooksAPI.update(book, event.target.value).then((response) => {
+      this.setState({
+        books: currentBooks,
+      });
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="bg-blue-400 py-3 px-0 text-center">
+          <h1 className="m-0 text-4xl font-medium text-white">MyReads</h1>
+        </div>
+
+        <div className="flex-1 px-0 pt-0 pb-0">
+          {/*Display the three different shelves in main pages with its current books */}
+          <BookShelf
+            key="currently"
+            books={this.props.currentBooks.filter(
+              (book) => book.shelf === "currentlyReading"
+            )}
+            updateShelf={this.updateShelf}
+            shelfTitle="Currently Reading"
+          />
+          <BookShelf
+            key="wantToRead"
+            books={this.props.currentBooks.filter(
+              (book) => book.shelf === "wantToRead"
+            )}
+            updateShelf={this.updateShelf}
+            shelfTitle="Want to Read"
+          />
+          <BookShelf
+            key="read"
+            books={this.props.currentBooks.filter(
+              (book) => book.shelf === "read"
+            )}
+            updateShelf={this.updateShelf}
+            shelfTitle="Read"
+          />
+        </div>
+        <div
+          className="w-50 h-50 fixed right-20 bottom-20 block 
+                            rounded-full bg-blue-400 bg-[url('./icons/add.svg')] bg-auto bg-no-repeat shadow-lg "
+        >
+          <Link to="/search" className="text-white opacity-0">
+            Add a
+            <br />
+            Button
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BookList;
