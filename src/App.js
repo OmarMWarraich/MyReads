e19ff7b90200f9f5b66106c5e975ccca8bd8c6be
OmarMWarraich <earthling986@gmail.com>
2022-02-07 18:57:03 +0500

    logged data
    Read Api data in the console.log to record its props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
-function App() {
-  return <div className="text-4xl underline">hello</div>;
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import * as BooksAPI from "./BooksAPI";
+
+
+class App extends Component {
+  state = {
+    books: []
+  };
+
+  componentDidMount() {
+    this.updateData();
+  }
+  
+  updateShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf).then(response => {
+      this.updateData();
+    })
+  }
+  updateData = () => {
+    BooksAPI.getAll().then(data => console.log(data));
+  }
+
+
+render() {
+  return (
+    <div></div>
+  )
 }
+
+};
+
 
 export default App;
